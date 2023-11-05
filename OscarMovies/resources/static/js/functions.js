0cee16fc8ba4b0429d6d82016d7650df07785ed9
nlkishore <nlaxmikishore@gmail.com>
2023-11-05 14:42:44 +0800

    Delete OscarMovies/resources directory

--- OscarMovies/resources/static/js/functions.js
+++
@@ -1,5 +0,0 @@
-$(document).on('change','#genresselect' ,function(){
-  var val = $('#genresselect option:selected').val();
-  var text = $('#genresselect option:selected').text();
-  console.log(' value is '+val +' Text is '+text)
-})
