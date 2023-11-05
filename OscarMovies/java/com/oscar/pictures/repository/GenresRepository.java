e9c8d82dbe576eb79c9a455960bb207e486b19f5
nlkishore <nlaxmikishore@gmail.com>
2023-11-05 14:48:34 +0800

    Delete OscarMovies/java/com/oscar/pictures directory

--- OscarMovies/java/com/oscar/pictures/repository/GenresRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oscar.pictures.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oscar.pictures.entity.GenRes;
-
-public interface GenresRepository extends JpaRepository<GenRes, String> {
-
-}
--- OscarMovies/java/com/oscar/pictures/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oscar.pictures.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oscar.pictures.entity.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie, String> {
-
-}
